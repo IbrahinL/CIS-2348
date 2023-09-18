34c70e73d8457869bd302cc9dd7605b765c096d3
Ibrahin Leon <143020157+IbrahinL@users.noreply.github.com>
2023-09-17 23:25:10 -0500

    zybooks code 2.2

--- Zylab_code_1.2/Dummy.py
+++ Zylab_code_1.2/Dummy.py
@@ -1,1 +1,11 @@
-
+user_num = int(input("Enter integer:\n"))
+user_squared = user_num * user_num
+user_cubed = user_num * user_num * user_num
+print("You entered:", user_num)
+print(user_num, "squared is", user_squared)
+print("And", user_num, "cubed is", user_cubed, "!!")
+user_num2 = int(input("Enter another integer:\n"))
+Addition = user_num + user_num2
+Multiplication = user_num * user_num2
+print(user_num, "+", user_num2, "is", Addition)
+print(user_num, "*", user_num2, "is", Multiplication)
