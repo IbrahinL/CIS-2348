90e891dcff82bb9659d90646bdd30b343fe0bffd
Ibrahin Leon <143020157+IbrahinL@users.noreply.github.com>
2023-10-09 23:57:30 -0500

    Committing problem 2

---
+++ Homework-2/Coding_problem_2.py
@@ -0,0 +1,32 @@
+import re
+
+def is_valid_date(date_str):
+    # Check if the date string matches the required format
+    pattern = r"^(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2},\s\d{4}$"
+    return re.match(pattern, date_str) is not None
+
+def parse_date(date_str):
+    monthDict = {"January" : 1, "February" : 2, "March" : 3, "April" : 4,
+                 "May" : 5, "June" : 6, "July" : 7, "August" : 8, "September" : 9,
+                 "October" : 10, "November" : 11, "December" : 12}
+
+    # Extract the month, day, and year from the date string using find()
+
+    year = date_str[date_str.find(',') + 2 : ]
+
+    month, day = date_str[ : date_str.find(',')].split(" ")
+
+    month = monthDict[month.title()]
+
+    return '{}/{}/{}'.format(month, day, year)
+
+#Part A: Read dates from input until -1 is entered
+print("Enter dates (format: Month Day, Year):")
+dates = []
+while True:
+    date = input()
+    if date == '-1':
+        break
+    if is_valid_date(date):
+         dates.append(parse_date(date))
+         print(dates)
